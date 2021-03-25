0fef7372ffaf903d085d14112528114d04e05ce7
Thomas Mao <tm1813@gmail.com>
2021-03-25 23:25:47 +0800

    accessing a property in spring boot

--- src/main/java/com/dineshkrish/cms/api/ErrorHandler.java
+++ src/main/java/com/dineshkrish/cms/api/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.dineshkrish.cms.api;
 
 import com.dineshkrish.cms.exception.ApplicationError;
 import com.dineshkrish.cms.exception.CustomerNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,13 +15,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
+    @Value("${api_doc_url2}")
+    private String details;
+
     @ExceptionHandler(CustomerNotFoundException.class)
     public ResponseEntity<ApplicationError> handleCustomerNotFoundException(CustomerNotFoundException exception
             , WebRequest webRequest) {
 		ApplicationError error = new ApplicationError();
         error.setCode(101);
         error.setMessage(exception.getMessage());
-
+        error.setDetails(details);
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }

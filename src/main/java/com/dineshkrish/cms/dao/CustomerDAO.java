0bae6c677e00f98ace3a13860cbd37b5e2a7a6af
Thomas Mao <tm1813@gmail.com>
2021-03-25 21:56:13 +0800

    Adding a CRUD Repository in Spring Boot

--- src/main/java/com/dineshkrish/cms/dao/CustomerDAO.java
+++ src/main/java/com/dineshkrish/cms/dao/CustomerDAO.java
@@ -1,4 +1,9 @@
 package com.dineshkrish.cms.dao;
 
-public class CustomerDAO {
+import com.dineshkrish.cms.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDAO extends CrudRepository<Customer, Integer> {
 }

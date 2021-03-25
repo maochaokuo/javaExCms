0e206f67ae87ba7ef3b8f5f2869437c4f7bca463
Thomas Mao <tm1813@gmail.com>
2021-03-25 22:08:10 +0800

    CRUD operation part 1

--- src/main/java/com/dineshkrish/cms/service/CustomerService.java
+++ src/main/java/com/dineshkrish/cms/service/CustomerService.java
@@ -1,6 +1,8 @@
 package com.dineshkrish.cms.service;
 
+import com.dineshkrish.cms.dao.CustomerDAO;
 import com.dineshkrish.cms.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,16 +11,22 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class CustomerService {
+
+    @Autowired
+    private CustomerDAO customerDAO;
+
     private int customerIdCount = 1;
     private List<Customer> customerList=new CopyOnWriteArrayList<>();
     public Customer addCustomer(Customer customer){
-        customer.setCustomerId(customerIdCount);
-        customerList.add(customer);
-        customerIdCount++;
-        return customer;
+//        customer.setCustomerId(customerIdCount);
+//        customerList.add(customer);
+//        customerIdCount++;
+//        return customer;
+        return customerDAO.save(customer);
     }
     public List<Customer> getCustomers(){
-        return customerList;
+        return customerDAO.findAll();
+//        return customerList;
     }
     public Customer getCustomer(int customerId){
         return customerList
